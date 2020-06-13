f62fcd97e5736d0b84c208bcc9508935a7de5490
kang yewon <kyeah01@gmail.com>
2020-06-13 23:04:48 +0900

    add main method

--- src/main/java/domain/Race.java
+++ src/main/java/domain/Race.java
@@ -1,5 +1,20 @@
 package domain;
 
-public class Race {
+import java.util.Scanner;
 
+
+public class Race {
+    public String[] getCarName() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        String[] carNames = scan.nextLine().split(",");
+        return carNames;
+    }
+
+    public int getRaceTime() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("시도할 회수는 몇회인가요?");
+        int count = scan.nextInt();
+        return count;
+    }
 }
