476409925fb4830e286314bce9d4e704dea9c174
kang yewon <kyeah01@gmail.com>
2020-06-15 16:39:46 +0900

    200615 | Refactor : devide method && set variable - devide 'get car name', 'get race time' method - set 'scanner' static final variable

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main (String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-        String[] carNames = sc.nextLine().split(",");
-        int countofCar = carNames.length;
-
-        System.out.println("시도할 회수는 몇회인가요?");
-        int count = sc.nextInt();
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+//        String[] carNames = sc.nextLine().split(",");
+//        int countofCar = carNames.length;
+//
+//        System.out.println("시도할 회수는 몇회인가요?");
+//        int count = sc.nextInt();
 
         Car[] cars = new Car[countofCar];
 
--- src/main/java/domain/Race.java
+++ src/main/java/domain/Race.java
@@ -2,19 +2,19 @@ package domain;
 
 import java.util.Scanner;
 
-
 public class Race {
+    private final static Scanner scan = new Scanner(System.in);
+
     public String[] getCarName() {
-        Scanner scan = new Scanner(System.in);
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
         String[] carNames = scan.nextLine().split(",");
         return carNames;
     }
 
     public int getRaceTime() {
-        Scanner scan = new Scanner(System.in);
         System.out.println("시도할 회수는 몇회인가요?");
         int count = scan.nextInt();
         return count;
     }
+
 }
